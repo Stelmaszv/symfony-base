02408b115f431615bdd98fa5060594dd7827851c
Stelmaszv <stelmaszv@gmial.com>
2024-04-04 13:55:51 +0200

    Add login in AuthenticationAPi

--- src/Generic/Auth/AuthenticationAPi.php
+++ src/Generic/Auth/AuthenticationAPi.php
@@ -3,9 +3,14 @@
 
 namespace App\Generic\Auth;
 
+use App\Entity\User;
 use Symfony\Flex\Response;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 trait AuthenticationAPi
 {
@@ -16,30 +21,54 @@ trait AuthenticationAPi
         $this->jwt = $jwt;
     }
 
-    #[Route(path: '/token', name: 'app_login')]
-    public function getToken() :JsonResponse
+    #[Route('/api/logout', name: 'app_logout')]
+    public function logout(): Response
     {
-        $tokenStorage = $this->container->get('security.token_storage');
-        $token = $tokenStorage->getToken();
-        if($token === null){
-            return new JsonResponse(['error'=> 'tokken requried'],JsonResponse::HTTP_UNAUTHORIZED);
-        }
-        $user = $token->getUser();
+        throw new \LogicException('This route should not be called directly.');
+    }
 
+    #[Route('/api/login', name: 'login', methods: ['POST'])]
+    public function login(Request $request,ManagerRegistry $doctrine): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return new JsonResponse(['message' => 'Invalid data Login'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $user = $doctrine?->getRepository(User::class)?->findOneBy(['email' => $data['email']]);
+
+        if (!$user) {
+            return new JsonResponse(['message' => 'Invalid data Login'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+     
+        if (!password_verify($data['password'], $user->getPassword())) {
+            return new JsonResponse(['message' => 'Invalid data Login or password'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        return new JsonResponse([
+            'token' => $this->generateToken($user)
+        ]);
+    }
+
+    private function isPasswordValid(UserInterface $user, string $password): bool
+    {
+        return password_verify($password, $user->getPassword());
+    }
+
+    private function generateToken(UserInterface $user): string
+    {
         $userPayload = [
             'id ' =>  $user->getId(),
             'email' => $user->getEmail(),
             'roles' => $user->getRoles()
         ];
 
-        return new JsonResponse([
-            'token' => $this->jwt->encode($userPayload)
-        ], JsonResponse::HTTP_OK);
+        return $this->jwt->encode($userPayload);
     }
 
-    #[Route('/logout', name: 'app_logout')]
-    public function logout(): Response
-    {
-        throw new \LogicException('This route should not be called directly.');
+    #[Route(path: '/login', name: 'app_login')]
+    public function loginAction(AuthenticationUtils $authenticationUtils){
+        throw new \LogicException('Login Action');
     }
 }
